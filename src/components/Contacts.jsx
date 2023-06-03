78c634f7b852e20611ca401f3c1816332a100653
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2023-06-03 10:29:04 +0500

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles} from "@material-ui/core/styles";
 import {
 
-
+TextField,
 Grid,
 
 Box
@@ -16,13 +16,40 @@ const useStyles=makeStyles((theme)=>({
         left:"50%",
         transform:"translate(-50%, -50%)",
         position:"absolute",
+        
     },
-    button:{
-        marginTop:"1rem",
-        color:"#B15DDD",
-        borderColor:"#B15DDD",
-    }
-}))
+    formField: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+        '& .MuiInputLabel-root': {
+            color: '#D5BCFA',
+          },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: '#D5BCFA',
+            },
+            '&:hover fieldset': {
+              borderColor: '#D5BCFA',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#D5BCFA',
+            },
+            '& input': {
+                color: 'white',
+              },
+          },
+         
+        },
+        
+        heading: {
+            textAlign: 'center',
+            color: '#B15DDD',
+            marginBottom: theme.spacing(2),
+            fontWeight: 300,
+            fontSize: '27px',
+          },
+ 
+}));
 
 
 const Contacts = () => {
@@ -34,12 +61,31 @@ const Contacts = () => {
           
             <Box component="form" className={classes.form}>
             <form action="https://send.pageclip.co/KRUQS5No9DJqo4Y1Vblb0D3Bpbs00iQr" className="pageclip-form" method="post">
+            <h2 className={classes.heading}>HIRE OR CONTACT ME</h2>
 
-  <input type="text" name="name" value="Roscoe Jones" />
-  <input type="email" name="email" value="roscoe@example.com" />
+            <TextField
+        className={classes.formField}
+        label="Name"
+        variant="outlined"
+      />
 
-  
-  <button type="submit" className="pageclip-form__submit">
+      <TextField
+        className={classes.formField}
+        label="Email"
+        variant="outlined"
+        type="email"
+      />
+    
+  <button 
+  type="submit" 
+  className="pageclip-form__submit" 
+  style={{ backgroundColor: 'transparent',
+  color: '#B15DDD', 
+  padding:"10px 20px",
+  borderRadius: '5px',
+  borderColor: '#D5BCFA',
+  width:'100%',
+  fontSize:'18px' }}>
     <span>Send</span>
   </button>
 </form>
